9f7d3c29e56c6b28e21bec76ad64f0318bd494aa
Paul Bamberg <jpbamberg1993@gmail.com>
2024-03-02 09:53:27 -0500

    Create section for self assessment

--- src/components/sections/reading-material-section.tsx
+++ src/components/sections/reading-material-section.tsx
@@ -1,12 +1,28 @@
+'use client'
 import {GradientWrapper} from '@/components/gradient-wrapper'
 import {TextTitle} from '@/components/text-title'
 import {BookCarousel} from '@/components/book-carousel'
+import {MouseEvent} from 'react'
 
 export function ReadingMaterialSection() {
+	function handleClick(e: MouseEvent<HTMLButtonElement>) {
+		e.preventDefault()
+		window.location.href = 'https://www.aa.org/self-assessment'
+	}
+
 	return (
 		<GradientWrapper>
 			<TextTitle text="Reading Material" />
 			<BookCarousel />
+			<div className="flex flex-col justify-center pb-2 pt-12">
+				<h4 className="pb-4 text-center text-xl font-medium text-white">Not sure if you have a problem?</h4>
+				<button
+					onClick={handleClick}
+					className="mx-auto w-[225px] rounded-lg bg-white py-1 text-sm font-semibold text-sea-green"
+				>
+					Take a Self-Assessment
+				</button>
+			</div>
 		</GradientWrapper>
 	)
 }
