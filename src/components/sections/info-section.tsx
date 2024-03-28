eefd3a2318a234db1d4679d0a63c818f143be566
Paul Bamberg <jpbamberg1993@gmail.com>
2024-03-28 12:36:41 -0500

    Implement traditions button

--- src/components/sections/info-section.tsx
+++ src/components/sections/info-section.tsx
@@ -5,25 +5,27 @@ export function InfoSection() {
 	return (
 		<section
 			className="relative mt-4 h-72 w-full bg-light-grey px-6 pt-4"
-			id="traditions"
+			id="info"
 		>
 			<Image
 				src="/fellowship.svg"
 				alt="a group of people standing closely"
 				width="640"
 				height="195"
-				className="absolute inset-0 bg-cover bg-center bg-no-repeat pt-8 opacity-20"
+				className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat pt-8 opacity-20"
 			/>
-			<h2 className="text-3xl font-bold text-ocean-blue">
-				You are
-				<br />
-				<span className="font-extrabold">NOT alone.</span>
-			</h2>
-			<p className="pr-16 pt-2 text-sm text-black">
-				Alcoholics Anonymous is a fellowship of men and women who share their experience, strength and hope with
-				each other that they may solve their common problem and help others to recover from alcoholism.
-			</p>
-			<TraditionsBtn />
+			<div className="relative z-20">
+				<h2 className="text-3xl font-bold text-ocean-blue">
+					You are
+					<br />
+					<span className="font-extrabold">NOT alone.</span>
+				</h2>
+				<p className="pr-16 pt-2 text-sm text-black">
+					Alcoholics Anonymous is a fellowship of men and women who share their experience, strength and hope
+					with each other that they may solve their common problem and help others to recover from alcoholism.
+				</p>
+				<TraditionsBtn />
+			</div>
 		</section>
 	)
 }
--- src/components/traditions-btn.tsx
+++ src/components/traditions-btn.tsx
@@ -2,9 +2,17 @@
 import {ButtonOutline} from '@/components/button-outline'
 
 export function TraditionsBtn() {
+	const navigateToTraditions = () => {
+		const target = document.querySelector('#traditions')
+		if (target) {
+			target.scrollIntoView({behavior: 'smooth'})
+		} else {
+			console.error('Could not find the target element')
+		}
+	}
 	return (
 		<div className="pt-4">
-			<ButtonOutline onClick={() => console.log('You clicked me')}>12 Traditions</ButtonOutline>
+			<ButtonOutline onClick={navigateToTraditions}>12 Traditions</ButtonOutline>
 		</div>
 	)
 }
