60bce3fdd23dfc8bec73ca7d7826a56b85ec7f4e
Paul Bamberg <jpbamberg1993@gmail.com>
2024-03-28 12:48:32 -0500

    Ensure text is white on mobile

--- src/components/sections/meetings-section.tsx
+++ src/components/sections/meetings-section.tsx
@@ -9,14 +9,14 @@ export function MeetingsSection() {
 			<TextTitle text="MEETINGS" />
 			<h4 className="pt-2 text-xl font-semibold text-white">FACE TO FACE</h4>
 			<p className="font-light leading-4 text-white">Good Shepherd Group</p>
-			<p className="pt-2 font-medium">
+			<p className="pt-2 font-medium text-white">
 				Tuesday / Thursday: 7:00pm - 8:00pm
 				<br />
 				Saturday: 11:00am
 			</p>
 			<a
 				href="https://www.google.com/maps/dir//Av.+Sta.+Cruz+491,+Miraflores+15074,+Peru/@-12.109077,-77.0384462,15.87z/data=!4m8!4m7!1m0!1m5!1m1!1s0x9105c8400bbac97b:0xe31c83c887391f17!2m2!1d-77.0346723!2d-12.1078656?entry=ttu"
-				className="block pb-1 pt-5 font-medium"
+				className="block pb-1 pt-5 font-medium text-white"
 				target="_blank"
 			>
 				Anglican Cathedral of the Good Shepherd, Avenida Santa Cruz 491, Miraflores
@@ -24,7 +24,7 @@ export function MeetingsSection() {
 			<LocationMap />
 			<h4 className="pt-5 text-xl font-semibold text-white">ZOOM</h4>
 			<p className="font-light leading-4 text-white">Friends of Bill in Lima</p>
-			<p className="pt-2 font-medium">
+			<p className="pt-2 font-medium text-white">
 				Monday / Wednesday / Friday: 12:00pm
 				<br />
 				Tuesday: 7:00pm
